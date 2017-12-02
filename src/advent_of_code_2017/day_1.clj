84be6af111002e5ad9ca7e03349a60fd8e4c8733
Stuart White <s.white.1@elsevier.com>
2017-12-02 08:36:55 +0000

    Solution to day one part two

--- src/advent_of_code_2017/day_1.clj
+++ src/advent_of_code_2017/day_1.clj
@@ -1,6 +1,11 @@
 (ns advent-of-code-2017.day-1
-  (:require [clojure.java.io :as io]
+  (:require [advent-of-code-2017.utils :refer [def-]]
+            [clojure.java.io :as io]
             [clojure.string :as string]))
+
+(def captcha
+  (string/trim (slurp (io/resource "day-1-input.txt"))))
+
 
 (defn- digit-seq [x]
   (->> x
@@ -8,20 +13,29 @@
       (seq)
       (map #(Integer/parseInt (str %)))))
 
-(defn- digit-pairs [x]
+(defn pair-adjacent [x]
   (let [digits (digit-seq x)]
     (->> (interleave digits (drop 1 (cycle digits)))
-         (partition 2)
-         )))
+         (partition 2))))
 
-(defn sum-of-digits [x]
-  (->> (digit-pairs x)
+(defn sum-of-digits [pair-fn x]
+  (->> (pair-fn x)
        (filter #(apply = %))
        (map first)
        (apply +)))
 
-(def captcha
-  (string/trim (slurp (io/resource "day-1-input.txt"))))
-
 (defn solution-part-one []
-  (sum-of-digits captcha))
+  (sum-of-digits pair-adjacent captcha))
+
+;; Part two
+
+(defn pair-halfway [x]
+  (let [digits (digit-seq x)
+        n      (/ (count digits) 2)]
+    (->> (interleave digits (drop n (cycle digits)))
+         (partition 2))))
+
+(defn solution-part-two []
+  (sum-of-digits pair-halfway captcha))
+
+(solution-part-two)
--- test/advent_of_code_2017/day_1_test.clj
+++ test/advent_of_code_2017/day_1_test.clj
@@ -2,8 +2,15 @@
   (:require [advent-of-code-2017.day-1 :refer :all]
             [clojure.test :refer :all]))
 
-(deftest sum-of-digits-given-expected-input-then-expected-result
-  (is (= 3 (sum-of-digits 1122)))
-  (is (= 4 (sum-of-digits 1111)))
-  (is (= 0 (sum-of-digits 1234) 0))
-  (is (= 9 (sum-of-digits 91212129))))
+(deftest sum-of-digits-given-part-one-expected-input-then-expected-result
+  (is (= 3 (sum-of-digits pair-adjacent 1122)))
+  (is (= 4 (sum-of-digits pair-adjacent 1111)))
+  (is (= 0 (sum-of-digits pair-adjacent 1234) 0))
+  (is (= 9 (sum-of-digits pair-adjacent 91212129))))
+
+(deftest sum-of-digits-given-part-two-expected-input-then-expected-result
+  (is (= 6 (sum-of-digits pair-halfway 1212)))
+  (is (= 0 (sum-of-digits pair-halfway 1221)))
+  (is (= 4 (sum-of-digits pair-halfway 123425)))
+  (is (= 12 (sum-of-digits pair-halfway 123123)))
+  (is (= 4 (sum-of-digits pair-halfway 12131415))))
