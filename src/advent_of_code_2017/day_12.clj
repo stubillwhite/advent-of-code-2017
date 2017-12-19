9d99399ff8e63a99d90bd39b8d25a515f59c83ab
Stuart White <s.white.1@elsevier.com>
2017-12-19 07:17:32 +0000

    Solution to day twelve part one

---
+++ src/advent_of_code_2017/day_12.clj
@@ -0,0 +1,38 @@
+(ns advent-of-code-2017.day-12
+  (:require [advent-of-code-2017.utils :refer [def-]]
+            [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.set :as set]))
+
+(defn- split-newline [s]
+   (string/split s #"\n"))
+   
+(def- programs
+  (->> (io/resource "day-12-input.txt")
+       (slurp)
+       (string/trim)
+       (split-newline)))
+
+(defn- parse-line [s]
+  (let [to-int             (fn [x] (Integer/parseInt x))
+        pattern            #"^(\d+) <-> (.+)$"
+        [_ node nodes-csv] (re-find pattern s)]
+    [(to-int node)
+     (into #{} (map to-int (string/split nodes-csv #", ")))]))
+
+(defn construct-graph [lines]
+  (into {} (map parse-line lines)))
+
+(defn connected-to [graph node]
+  (letfn [(connected-to-iter [group conns]
+            (if (empty? conns)
+              group
+              (let [new-group (set/union group conns)
+                    new-conns (reduce set/union #{} (vals (select-keys graph conns)))]
+                (recur new-group (set/difference new-conns new-group)))))]
+    (connected-to-iter #{node} (graph node))))
+
+(defn solution-part-one []
+  (count (connected-to (construct-graph programs) 0)))
+
+
---
+++ test/advent_of_code_2017/day_12_test.clj
@@ -0,0 +1,30 @@
+(ns advent-of-code-2017.day-12-test
+  (:require [advent-of-code-2017.day-12 :refer :all]
+            [advent-of-code-2017.utils :refer [def-]]
+            [clojure.test :refer :all]))
+
+(def- example-input
+  ["0 <-> 2"
+   "1 <-> 1"
+   "2 <-> 0, 3, 4"
+   "3 <-> 2, 4"
+   "4 <-> 2, 3, 6"
+   "5 <-> 6"
+   "6 <-> 4, 5"])
+
+(def- example-graph
+  {0 #{2}
+   1 #{1}
+   2 #{0 3 4}
+   3 #{2 4}
+   4 #{2 3 6}
+   5 #{6}
+   6 #{4 5}})
+
+(deftest construct-graph-given-example-input-then-example-graph
+  (is (= example-graph (construct-graph example-input))))
+
+(deftest connected-to-given-example-graph-then-connected-nodes
+  (let [graph (construct-graph example-input)]
+    (is (= #{1}           (connected-to graph 1)))
+    (is (= #{0 2 3 4 5 6} (connected-to graph 0)))))
