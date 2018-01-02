f292d16b9fd2c25512a0d3a5890a0d63dbc5ba40
Stuart White <s.white.1@elsevier.com>
2018-01-02 21:31:37 +0100

    Solution to day fourteen part two

--- src/advent_of_code_2017/day_14.clj
+++ src/advent_of_code_2017/day_14.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2017.day-14
   (:require [advent-of-code-2017.day-10 :as day-10]
-            [advent-of-code-2017.utils :refer [def-]]))
+            [advent-of-code-2017.utils :refer [def-]]
+            [clojure.set :as set]))
 
 (def key-string "wenycdww")
 
@@ -25,3 +26,54 @@
 
 (defn solution-part-one []
   (count-used-squares (binary-grid key-string)))
+
+;; Part two
+
+;; Convert to a map and flood-fill each used square
+
+(defn index-grid [grid]
+  (into {}
+        (for [x (range (.length (first grid)))
+              y (range (count grid))]
+          [[x y] (nth (nth grid y) x)])))
+
+(defn index-binary-grid [grid]
+  (->> (index-grid grid)
+       (filter (fn [[k v]] (= v \1)))
+       (into {})))
+
+(defn- neighbours [points]
+  (into #{}
+        (for [[x y]   points
+              [dx dy] [[0 1] [-1 0] [1 0] [0 -1]]]
+          [(+ x dx) (+ y dy)])))
+
+(defn- drop-keys [map ks]
+  (reduce dissoc map ks))
+
+(defn- flood-fill [points grid]
+  (let [new-points (into #{} (keys (select-keys grid (neighbours points))))]
+    (if (empty? new-points)
+      points
+      (flood-fill (set/union points new-points)
+                  (drop-keys grid new-points)))))
+
+(defn connected-groups [grid]
+  (letfn [(groups-iter [groups grid]
+            (if (empty? grid)
+              groups
+              (let [new-group (flood-fill #{(first (keys grid))} grid)]
+                (recur (conj groups new-group)
+                       (drop-keys grid new-group)))))]
+    (groups-iter #{} grid)))
+
+(defn count-of-connected-groups [key-string]
+  (->> (binary-grid key-string)
+       (index-binary-grid)
+       (connected-groups)
+       (count)))
+
+(defn solution-part-two []
+  (count-of-connected-groups key-string))
+
+
--- test/advent_of_code_2017/day_14_test.clj
+++ test/advent_of_code_2017/day_14_test.clj
@@ -24,3 +24,26 @@
 
 (deftest count-used-squares-given-example-input-then-example-output
   (is (= 8108 (count-used-squares (binary-grid example-key-string)))))
+
+(deftest index-grid-given-grid-then-indexed-content
+  (let [grid     ["123"
+                  "456"
+                  "789"]
+        expected {[0 0] \1 [1 0] \2 [2 0] \3
+                  [0 1] \4 [1 1] \5 [2 1] \6
+                  [0 2] \7 [1 2] \8 [2 2] \9}]
+    (is (= expected (index-grid grid)))))
+
+(deftest connected-groups-given-groups-exist-then-identifies-groups
+  (let [raw-grid ["0011"
+                  "0001"
+                  "1110"
+                  "0001"]
+        grid     (index-binary-grid raw-grid)
+        expected #{#{[2 0] [3 0] [3 1]}
+                   #{[0 2] [1 2] [2 2]}
+                   #{[3 3]}}]
+    (is (= expected (connected-groups grid)))))
+
+(deftest count-of-connected-groups-given-example-input-then-example-count
+  (is (= 1242 (count-of-connected-groups example-key-string))))
